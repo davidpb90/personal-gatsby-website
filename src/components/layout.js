2872a76b414274c263549fa5f6eced6ab2b2e5be
David Pardo <davidpb90@gmail.com>
2023-08-07 23:47:14 +0200

    added sections and styling to the Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,13 @@ import {
     navLinkItem,
     navLinkText,
     siteTitle,
+    wrapper,
+    mainContainer,
+    main,
+    rightNavbar,
+    brand,
+    navbar,
+    footer
 } from './layout.module.scss'
 
 const Layout = ({ pageTitle, children }) => {
@@ -21,32 +28,82 @@ const Layout = ({ pageTitle, children }) => {
     `)
     
     return (
-        <div className={container}>
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}>
-                        <Link to="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/about">
-                            About
-                        </Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/blog" className={navLinkText}>
-                            Blog
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>
-                <h1 className={heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+        <div className={wrapper}>
+	  		<div className={mainContainer}>
+	    		<main className={main}>
+                    <h1 className={heading}>{pageTitle}</h1>
+                    {children}
+	    			{/* <h1 className={text}-left py-1 px-2 mx-2">David Pardo Bernal</h1> */}
+	    			{/* <p className={lead} py-1 px-2 mx-2">A personal and professional repository.</p> */}
+	    			{/* <p className={lead}"> */}
+	    			{/* </p> */}
+	    		    {/* <script src="js/p5-sketches.js"></script> */}
+	    		</main>
+	    		<div className={rightNavbar}>
+	    		  	<h3 className={brand}>DPB</h3>
+	    		  	{/* <div className={navbarToggle}>
+	    		  		<i className={fas}></i>
+	    		  	</div> */}
+	    		  	<nav className={navbar}>
+                        <ul className={navLinks}>
+	    		    		<li className={navLinkItem}> 
+                                <Link to="/" className={navLinkText}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/about" className={navLinkText}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/blog" className={navLinkText}>
+                                    Blog
+                                </Link>
+                            </li>
+	    		    		{/* <li><a className={navLink} href="data.html">Data</a></li>
+	    		    		<li><a className={navLink} href="stories.html">Stories</a></li>
+	    		    		<li><a className={navLink} href="photography.html">Photography</a></li>
+	    		    		<li><a className={navLink} href="travels.html">Travels</a></li>
+	    		    		<li><a className={navLink} href="experiments.html">Experiments</a></li>
+	    		    		<li><a className={navLink} href="cv.html">CV</a></li>
+	    		    		<li><a className={navLink} href="contact.html">Contact</a></li> */}
+	    		    	</ul>
+	    		  	</nav>
+	    		</div>
+	  		</div>	
+			<footer className={footer}>
+				<a href="contact.html">Contact Me</a>
+				<p>Designed and built by David Pardo Bernal based on Bootstrap's Cover template</p>
+	  	    </footer>
+	  	</div>
+
+        // <div className={container}>
+        //     <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        //     <nav>
+        //         <ul className={navLinks}>
+        //             <li className={navLinkItem}>
+        //                 <Link to="/">
+        //                     Home
+        //                 </Link>
+        //             </li>
+        //             <li className={navLinkItem}>
+        //                 <Link to="/about">
+        //                     About
+        //                 </Link>
+        //             </li>
+        //             <li className={navLinkItem}>
+        //                 <Link to="/blog" className={navLinkText}>
+        //                     Blog
+        //                 </Link>
+        //             </li>
+        //         </ul>
+        //     </nav>
+        //     <main>
+        //         <h1 className={heading}>{pageTitle}</h1>
+        //         {children}
+        //     </main>
+        // </div>
     )
 }
 
