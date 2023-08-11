849ad0e5eab1fd07a4adeca79e3ba998b9a98138
David Pardo <davidpb90@gmail.com>
2023-08-11 23:37:00 +0200

    added my own p5 sketch

--- src/components/p5-first-sketch.js
+++ src/components/p5-first-sketch.js
@@ -56,40 +56,88 @@
 //     isDiffusing = false;
 // }
 
+// import React from 'react';
+// import Sketch from 'react-p5';
+
+// var x = 50;
+// var speed = 5;
+// function P5Sketch() {
+//     const setup = (p5, canvasParentRef) => {
+//         p5.createCanvas(500, 400).parent(canvasParentRef);
+//     }
+
+//     const draw = (p5) => {
+//         p5.frameRate(30);
+//         // If we're travelling towards the right or left
+//         if (speed > 0) {
+//             // If the ball has reached the end of the container or not
+//             if (x + 50 < p5.width) {
+//                 x += speed
+//             } else {
+//                 speed = -speed;
+//             }
+//         } else {
+//             if (x - 50 > 0) {
+//                 x += speed;
+//             } else {
+//                 speed = -speed;
+//             }
+//         }
+
+//         p5.background(255, 120, 20);
+//         p5.ellipse(x, 100, 100);
+//     }
+
+//     return (
+//         <Sketch setup={setup} draw={draw} />
+//     )
+// }
+
+// export default P5Sketch;
+
+
 import React from 'react';
 import Sketch from 'react-p5';
 
-var x = 50;
-var speed = 5;
+let circleRadius = 50;
+let maxRadius = 300;
+let isDiffusing = false;
 function P5Sketch() {
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(500, 400).parent(canvasParentRef);
     }
 
     const draw = (p5) => {
-        p5.frameRate(30);
-        // If we're travelling towards the right or left
-        if (speed > 0) {
-            // If the ball has reached the end of the container or not
-            if (x + 50 < p5.width) {
-                x += speed
-            } else {
-                speed = -speed;
+        //p5.frameRate(30);
+        p5.clear();
+        // background(255);
+        let c = p5.color(255, 204, 0);
+        p5.fill(c);
+        if (isDiffusing) {
+            circleRadius += 2; // Increase the radius to simulate diffusion
+            if (circleRadius > maxRadius) {
+                circleRadius = maxRadius;
             }
         } else {
-            if (x - 50 > 0) {
-                x += speed;
-            } else {
-                speed = -speed;
+            circleRadius -= 2; // Decrease the radius to revert diffusion
+            if (circleRadius < 0) {
+                circleRadius = 0;
             }
         }
+        let x = p5.width / 2;
+        let y = p5.height / 2;
+        p5.ellipse(p5.mouseX, p5.mouseY, circleRadius * 2);
+    }
 
-        p5.background(255, 120, 20);
-        p5.ellipse(x, 100, 100);
+    const mousePressed = (p5) => {
+        isDiffusing = true;
+    }
+    const mouseReleased = (p5) => {
+        isDiffusing = false;
     }
 
     return (
-        <Sketch setup={setup} draw={draw} />
+        <Sketch setup={setup} draw={draw} mousePressed={mousePressed} mouseReleased={mouseReleased}/>
     )
 }
 
