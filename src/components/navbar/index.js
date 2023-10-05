55605f3f651b725cda9de99bc5a0519226a5bac5
David Pardo <davidpb90@gmail.com>
2023-10-05 23:34:35 +0200

    added code to potentially create a navbar toggle

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -18,6 +18,53 @@ import {
     footer
 } from '../../scss/5-components/navbar.module.scss'
 
+// import logo from '../img/logo.svg';
+
+// class Navbar extends React.Component {
+
+//     state = { showMenu: false }
+
+//     toggleMenu = () => {
+//         this.setState({
+//             showMenu: !this.state.showMenu
+//         })
+//     }
+
+//     render() {
+//         const menuActive = this.state.showMenu ? 'is-active' : '';
+//         const burgerActive = this.state.showMenu ? 'is-active' : '';
+//         return (
+//             <nav className="navbar">
+//                 <div className="navbar-brand">
+//                     <Link className="navbar-item" to="/">
+//                         <img src={logo} style={{ width: '88px' }} itemprop="image" alt="" />
+//                     </Link>
+//                     <div className={`navbar-burger burger ${burgerActive}`} onClick={this.toggleMenu}>
+//                         <span></span>
+//                         <span></span>
+//                         <span></span>
+//                     </div>
+//                 </div>
+
+//                 <div className={`navbar-menu ${menuActive}`} >
+//                     <div className="navbar-start">
+//                         <Link className="navbar-link" to="/" onClick={this.toggleMenu}>
+//                             Home
+//                         </Link>
+//                         <Link className="navbar-link" to="/services" onClick={this.toggleMenu}>
+//                             Services
+//                         </Link>
+//                         <Link className="navbar-link" to="/contact" onClick={this.toggleMenu}>
+//                             Contact
+//                         </Link>
+//                     </div>
+//                 </div>
+//             </nav>)
+//     }
+// };
+
+// export default Navbar;
+
 const NavBar = ({ pageTitle, children }) => {
     const data = useStaticQuery(graphql`
         query MyTitleQuery {
