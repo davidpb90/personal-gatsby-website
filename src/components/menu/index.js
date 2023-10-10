bea5043b3ce9475d46d923a3647b89b76ddbff41
David Pardo <davidpb90@gmail.com>
2023-10-10 23:15:54 +0200

    continued adding a burger menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -6,7 +6,7 @@ import {
     heading,
     navLinks,
     navLinkItem,
-    navLinkText,
+    a,
     container__col1,
     siteTitle,
     wrapper,
@@ -20,103 +20,105 @@ import {
     bmBurgerButton,
     bmBurgerBars,
     bmBurgerBarsHover,
-    bmCrossButton
+    bmCrossButton,
+    nav
 } from '../../scss/5-components/menu.module.scss'
-import { slide as Menu } from "react-burger-menu"
-import '../../scss/5-components/menu.module.scss'
 
 
-
-export default props => {
+const Menu = () => {
     return (
-        // <Menu>
-        //     <div className={container__col1 + ' ' + rightNavbar}>
-	    // 		  	<h3 className={brand}>DPB</h3>
-	    // 		  	{/* <div className={navbarToggle}>
-	    // 		  		<i className={fas}></i>
-	    // 		  	</div> */}
-        //             <nav className={navbar}>
-        //                 <Header>
+     
+            <nav className={container__col1 + ' ' + nav}>
+	    		  	<h3 className={brand}>DPB</h3>
+	    		  	{/* <div className={navbarToggle}>
+	    		  		<i className={fas}></i>
+	    		  	</div> */}
+                    <div className={navbar}>
+                        <Header>
                         
-        //                 </Header>
-        //                 <ul className={navLinks}>
-	    // 		    		<li className={navLinkItem}> 
-        //                     <Link to="/" className={navLinkText}>
-        //                             Home
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/about" className={navLinkText}>
-        //                             About
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/blog" className={navLinkText}>
-        //                             Blog
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/travels" className={navLinkText}>
-        //                             Travels
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/photography" className={navLinkText}>
-        //                             Photography
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/stories" className={navLinkText}>
-        //                             Stories
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/cv" className={navLinkText}>
-        //                             CV
-        //                         </Link>
-        //                     </li>
-        //                     <li className={navLinkItem}>
-        //                         <Link to="/contact" className={navLinkText}>
-        //                             Contact
-        //                         </Link>
-        //                     </li>
+                        </Header>
+                        <ul className={navLinks}>
+	    		    		<li className={navLinkItem}> 
+                            <Link to="/" className={a}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/about" className={a}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/blog" className={a}>
+                                    Blog
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/travels" className={a}>
+                                    Travels
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/photography" className={a}>
+                                    Photography
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/stories" className={a}>
+                                    Stories
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/cv" className={a}>
+                                    CV
+                                </Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/contact" className={a}>
+                                    Contact
+                                </Link>
+                            </li>
 	    		    		
-	    // 		    	</ul>
-	    // 		  	</nav>
-	    // 		</div>
-        // </Menu>	
+	    		    	</ul>
+	    		  	</div>
+	    		</nav>
+          
+        )   
+    }
+    
+    export default Menu;
 
-        <Menu className={bmBurgerButton + ' ' + bmBurgerBars + ' ' + bmCross} {...props}>
-            {/* <div className={container__col1 + ' ' + rightNavbar}> */}
-            <a className={brand}>DPB</a>
-            <Header>
-            </Header>
-            <Link to="/" className="menu-item">
-                Home
-            </Link>
-            <Link to="/about" className="menu-item">
-                About
-            </Link>                       
-            <Link to="/blog" className="menu-item">
-                Blog
-            </Link>                       
-            <Link to="/travels" className="menu-item">
-                Travels
-            </Link>                       
-            <Link to="/photography" className="menu-item">
-                Photography
-            </Link>                        
-            <Link to="/stories" className="menu-item">
-                Stories
-            </Link>                       
-            <Link to="/cv" className="menu-item">
-                CV
-            </Link>                       
-            <Link to="/contact" className="menu-item">
-                Contact
-            </Link>
-        </Menu>
+        // <Menu className={bmBurgerButton + ' ' + bmBurgerBars + ' ' + bmCross} {...props}>
+        //     {/* <div className={container__col1 + ' ' + rightNavbar}> */}
+        //     <a className={brand}>DPB</a>
+        //     <Header>
+        //     </Header>
+        //     <Link to="/" className="menu-item">
+        //         Home
+        //     </Link>
+        //     <Link to="/about" className="menu-item">
+        //         About
+        //     </Link>                       
+        //     <Link to="/blog" className="menu-item">
+        //         Blog
+        //     </Link>                       
+        //     <Link to="/travels" className="menu-item">
+        //         Travels
+        //     </Link>                       
+        //     <Link to="/photography" className="menu-item">
+        //         Photography
+        //     </Link>                        
+        //     <Link to="/stories" className="menu-item">
+        //         Stories
+        //     </Link>                       
+        //     <Link to="/cv" className="menu-item">
+        //         CV
+        //     </Link>                       
+        //     <Link to="/contact" className="menu-item">
+        //         Contact
+        //     </Link>
+        // </Menu>
 
-    )
-};
+//     )
+// };
 
